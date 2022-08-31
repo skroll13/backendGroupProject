6e023403c332878d4029b6dcc892d8ee31f5c8b0
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-08-31 16:41:53 -0500

    end of day

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 
 const express = require('express')
 const app = express()
-const helmet = require('helmet')
-const passport = require('passport')
-require('./auth/passport-config')(passport)
-const cookieSession = require('cookie-session')
+// const helmet = require('helmet')
+// const passport = require('passport')
+// require('./auth/passport-config')(passport)
+// const cookieSession = require('cookie-session')
 const port = 3000
 
 app.use(express.static('public'))
-app.use(helmet())
+// app.use(helmet())
 app.set('view engine', 'ejs')
 
 //scrapers
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.use(
-  cookieSession({
-    name: 'session',
-    keys: ['ssfddgfsfsfsfsefsafs'],
-    maxAge: 14 * 24 * 60 * 60 * 1000
-  })
-)
+// app.use(
+//   cookieSession({
+//     name: 'session',
+//     keys: ['ssfddgfsfsfsfsefsafs'],
+//     maxAge: 14 * 24 * 60 * 60 * 1000
+//   })
+// )
 
 //middleware
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(passport.initialize())
+// app.use(passport.session())
 
 //routes
 app.use(require('./routes/index.js'))
