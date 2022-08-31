871c1dcee6363d173eaf47c7f5373dbfa6a35d99
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-08-31 11:56:58 -0500

    updated login file to resolve conflicts

--- init.bash
+++
@@ -1,4 +0,0 @@
-# sequelize model:generate --name users --attributes name:string,username:string,email:string
-# sequelize model:generate --name park_visited --attributes park_name:string,address:string,phone:string,date_visited:date
-# sequelize model:generate --name blurb --attributes author:string,park:string,date:date
-sequelize model:generate --name roles --attributes name:string
